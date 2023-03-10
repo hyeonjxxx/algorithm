91e40e88876d38b2fedd9372e1bf5459a7312266
hyeon <82308444+hyeonjxxx@users.noreply.github.com>
2023-03-10 15:51:21 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "         ,r'\"7\n" +   //  \" 큰따옴표,\n 줄바꿈
+                   "r`-_   ,'  ,/\n" +
+                   " \\. \". L_r'\n" +    // \\ \(역슬래시)
+                   "   `~\\/\n" +
+                   "      |\n" +
+                   "      |";
+
+        System.out.print(s);
+        
+    }
+
+}
