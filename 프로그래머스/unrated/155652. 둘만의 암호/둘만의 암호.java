f6ead3e54b5407a8c15358e3bb330f2cf9be276b
hyeon <82308444+hyeonjxxx@users.noreply.github.com>
2023-08-28 21:59:25 +0900

    [unrated] Title: 둘만의 암호, Time: 0.83 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);            
+            
+            for(int j=0; j<index; j++){
+                c++;
+                if(c > 'z'){
+                    c -= 26;
+                }
+                if(skip.contains(String.valueOf(c))){                                  
+                    j--;                    
+                }            
+                
+            }
+             sb.append(c);
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
